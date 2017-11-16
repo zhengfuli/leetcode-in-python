30fa0e34f26bbba74c33d1c4ea5ca93f6be3546d
zhengfuli <zhengfuli@bupt.edu.cn>
2017-11-16 22:32:00 +0800

    handled in string

--- n009_palindrome_number.py
+++ n009_palindrome_number.py
@@ -4,15 +4,4 @@ class PalindromeNumber(object):
         :param x:int 
         :return:bool
         """
-        if x < 0:
-            return False
-        div = 1
-        while x/div >= 10:
-            div *= 10
-        while x:
-            if x/div != x%10:
-                return False
-            x %= div
-            x /= 10
-            div /= 100
-        return True
+        return str(x) == str(x)[::-1]
