0858e3acd8e0a12eb40abe1c3abbd908c1fd2fb6
zhengfuli <zhengfuli@bupt.edu.cn>
2018-01-08 20:22:21 +0800

    215

---
+++ n215_kth_largest_element_in_an_array.py
@@ -0,0 +1,9 @@
+class FindKthLargest(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if nums:
+            return sorted(nums, reverse=True)[k - 1]
