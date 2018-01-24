436b4ebaafbc79f40bc3b54a73afb3cb2bc78290
zhengfuli <zhengfuli@bupt.edu.cn>
2018-01-25 00:38:31 +0800

    Binary search and Newton Iteration

--- n069_Sqrt(x).py
+++ n069_Sqrt(x).py
@@ -4,14 +4,25 @@ class MySqrt(object):
         :type x: int
         :rtype: int
         """
-        min = 0.0
-        mid = 0.0
-        max = num*1.0
-
-        while (max-min) > 0.0000001:
-            mid = (min+max)/2.0
-            if mid*mid >= num:
-                max = mid
-            else:
-                min = mid
-        return mid
+        # 1 binary search has similar performance as Newton's in low resolution requirement
+        # but it requires special handling when dealing with numbers between 0 and 1
+        # if x < 0:
+        #     return None
+        # if x == 1:
+        #     return x
+        # left, mid, right = 0, 0, x
+        # while mid != (left + right) >> 1:
+        #     mid = (left + right) >> 1
+        #     if mid ** 2 < x:
+        #         left = mid
+        #     elif mid**2 == x:
+        #         print mid
+        #     else:
+        #         right = mid
+        # return mid
+        if x == 1:
+            return x
+        res = x >> 1
+        while res**2 > x:
+            res = (res + x / res) >> 1
+        return res
