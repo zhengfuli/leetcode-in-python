c0b73b3b9ff5f35f8ba8c4a4925dc63833c98b46
zhengfuli <zhengfuli@bupt.edu.cn>
2018-05-08 23:55:24 +0800

    dynamic planning, similar to climb stairs

--- n062_unique_paths.py
+++ n062_unique_paths.py
@@ -6,8 +6,19 @@ class UniquePaths(object):
         :rtype: int
         """
         if m == 1 and n == 1:
-            path = [[1]]
-        if m > 1 and n == 1:
-            path = [[1] for i in range(m)]
-        if m == 1 and n > 1:
-            path = [[]]
+            paths = [[1]]
+        elif m == 1 and n > 1:
+            paths = [[1] * n]
+        elif m > 1 and n == 1:
+            paths = [[1] for i in xrange(m)]
+        else:
+            paths = [[0] * n for i in xrange(m)]
+            for i in xrange(0, n):
+                paths[0][i] = 1
+            for i in xrange(0, m):
+                paths[i][0] = 1
+            for i in xrange(1, m):
+                for j in xrange(1, n):
+                    paths[i][j] = paths[i-1][j] + paths[i][j-1]
+        return paths[m-1][n-1]
+
