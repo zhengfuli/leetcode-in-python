b40009c6a14c1d3119d3777132f5bfb3ad356a6a
zhengfuli <zhengfuli@bupt.edu.cn>
2018-06-01 23:45:02 +0800

    easy add binary

--- n067_add_binary.py
+++ n067_add_binary.py
@@ -5,4 +5,11 @@ class Solution:
         :type b: str
         :rtype: str
         """
-        for i in range(min(len(a), len(a)), -1, -1):
+        res = ''
+        i, j, plus = len(a) - 1, len(b) - 1, 0
+        while i >= 0 or j >= 0 or plus == 1:
+            plus += int(a[i]) if i >= 0 else 0
+            plus += int(b[j]) if j >= 0 else 0
+            res = str(plus % 2) + res
+            i, j, plus = i - 1, j - 1, plus / 2
+        return res
