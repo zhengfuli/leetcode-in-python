adb577965e823d3f11794d6a58dfacf84f93c4a9
zhengfuli <zhengfuli@bupt.edu.cn>
2018-06-14 10:24:41 +0800

    greedy!!!

---
+++ n122_best_time_to_buy_and_sell_stock_II.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_profit = 0
+        for i in range(1, len(prices)):
+            max_profit += max(0, prices[i]-prices[i-1])
+
+        return max_profit
