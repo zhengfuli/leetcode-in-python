bc4ff38bfdb76efd69c6ccb7e7221aa9918c5afd
zhengfuli <zhengfuli@bupt.edu.cn>
2018-06-26 22:13:56 +0800

    binary tree traversal

---
+++ n144_binary_tree_preorder_traversal.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+
+        def dfs(root):
+            if not root: return
+
+            res.append(root.val)
+
+            dfs(root.left)
+            dfs(root.right)
+
+        dfs(root)
+
+        return res
+
