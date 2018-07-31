3b1d6ef3ea3b1d54dbd1e281cded5a44857d49cb
zhengfuli <zhengfuli@bupt.edu.cn>
2018-07-31 21:50:01 +0800

    important searching

---
+++ n240_search_a_2D_matrix_II.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix: return False
+
+        x, y = 0, len(matrix[0]) - 1
+
+        while x < len(matrix) and y >= 0:
+            if matrix[x][y] == target:
+                return True
+            elif matrix[x][y] > target:
+                y -= 1
+            else:
+                x += 1
+
+        return False
