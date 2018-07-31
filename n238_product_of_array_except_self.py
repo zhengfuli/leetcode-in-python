83bf642ed8b5f324bc9ca27cad74533d5cf34dd3
zhengfuli <zhengfuli@bupt.edu.cn>
2018-07-31 21:28:43 +0800

    very interesting factoring from head and tail

---
+++ n238_product_of_array_except_self.py
@@ -0,0 +1,17 @@
+class Solution:
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = [1] * len(nums)
+
+        for i in range(1, len(nums)):
+            res[i] = res[i-1] * nums[i-1]
+
+        right = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= right
+            right *= nums[i]
+
+        return res
