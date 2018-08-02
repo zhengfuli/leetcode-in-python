3c2aa680cb1c9c68a8bc741ee5fc0360e3b360e4
zhengfuli <zhengfuli@bupt.edu.cn>
2018-08-02 16:42:08 +0800

    nothing special

--- n263_ugly_number.py
+++ n263_ugly_number.py
@@ -4,8 +4,9 @@ class Solution:
         :type num: int
         :rtype: bool
         """
-        while num % 2 == 0: num /= 2
-        while num % 3 == 0: num /= 3
-        while num % 5 == 0: num /= 5
+
+        while num and num % 2 == 0: num /= 2
+        while num and num % 3 == 0: num /= 3
+        while num and num % 5 == 0: num /= 5
 
         return num == 1
