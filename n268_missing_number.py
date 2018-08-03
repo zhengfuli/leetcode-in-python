6443f0340002f707fdead7ea8635793e3af433b9
zhengfuli <zhengfuli@bupt.edu.cn>
2018-08-03 13:47:04 +0800

    map value to position, very classic

---
+++ n268_missing_number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.append('m')
+
+        for i in range(len(nums)):
+            if nums[i] == 'm': pass
+
+            if nums[i] != i:
+                nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
+
+        return nums.index('m')
