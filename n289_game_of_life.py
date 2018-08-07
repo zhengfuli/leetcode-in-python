1ea874644cb92655c66ba121c994c34984946bea
zhengfuli <zhengfuli@bupt.edu.cn>
2018-08-07 23:09:26 +0800

    traversal the board

---
+++ n289_game_of_life.py
@@ -0,0 +1,40 @@
+class Solution:
+    def gameOfLife(self, board):
+        """
+        :type board: List[List[int]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        for i in range(len(board) - 1):
+            for j in range(len(board[i]) - 1):
+                # check only partial neighbors
+                for nei_x, nei_y in [(i, j+1), (i+1, j+1), (i+1, j), (i+1, j-1)]:
+                    # validate neighbors within boundaries
+                    if 0 <= nei_x < len(board) and  0 <= nei_y < len(board[j]):
+                        # if this cell is dead
+                        if not board[i][j]:
+                            if board[nei_x][nei_y] > 0:
+                                board[i][j] -= 1
+                        else:
+                            if board[nei_x][nei_y] > 0:
+                                board[i][j] += 1
+                                # this cell is live, thus to its neighbor, this is a live neighbor
+                                board[nei_x][nei_y] += 1
+                            else:
+                                board[nei_x][nei_y] -= 1
+                print(board)
+
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if 0 < board[i][j] < 3: board[i][j] = 0
+                elif 3 <= board[i][j] <= 4: board[i][j] = 1
+                elif board[i][j] > 4: board[i][j] = 0
+                elif board[i][j] == -3: board[i][j] = 1
+                else: board[i][j] = 0
+
+tb = Solution()
+tb.gameOfLife([
+  [0,1,0],
+  [0,0,1],
+  [1,1,1],
+  [0,0,0]
+])
