7e510eff6aa96eadfc95182329e3ac02dd5d8f13
zhengfuli <zhengfuli@bupt.edu.cn>
2018-08-26 00:04:17 +0800

    traversal from leaf to root

--- n310_minimum_height_trees.py
+++ n310_minimum_height_trees.py
@@ -5,19 +5,25 @@ class Solution:
         :type edges: List[List[int]]
         :rtype: List[int]
         """
+        if n < 2: return [0]
+
         dict = {}
 
         for edge in edges:
             for i, node in enumerate(edge):
                 if node not in dict:
-                    dict[node] = [edge[i-1]]
+                    dict[node] = [edge[i - 1]]
                 else:
-                    dict[node].append(edge[i-1])
-
+                    dict[node].append(edge[i - 1])
+        # print(dict)
         while len(dict) > 2:
+            leaf = []
             for key in list(dict.keys()):
                 if len(dict[key]) == 1:
-                    dict[dict[key][0]].remove(key)
-                    del dict[key]
+                    leaf.append([key, dict[key][0]])
 
-        return len(dict.keys())
+            for pair in leaf:
+                dict[pair[1]].remove(pair[0])
+                del dict[pair[0]]
+
+        return list(dict.keys())
